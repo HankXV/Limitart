61ed658b12a6558321a135319c7fbfa1e1e1885d
HankXV <104381832@qq.com>
2017-07-03 17:18:00 +0800

    merge security relatives

--- src/com/limitart/util/exception/InvalidBase64CharacterException.java
+++ src/com/limitart/util/exception/InvalidBase64CharacterException.java
@@ -1,6 +1,6 @@
 package com.limitart.util.exception;
 
-public class InvalidBase64CharacterException extends IllegalArgumentException {
+public class InvalidBase64CharacterException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidBase64CharacterException(String message) {
