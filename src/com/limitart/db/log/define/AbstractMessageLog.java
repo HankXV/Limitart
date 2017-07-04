dc94ab2044f39ee8880cb51f2dffae50c8a1bf84
HankXV <104381832@qq.com>
2017-07-04 17:05:14 +0800

    README

--- src/com/limitart/db/log/define/AbstractMessageLog.java
+++
@@ -1,9 +0,0 @@
-package com.limitart.db.log.define;
-
-import com.limitart.db.log.anotation.LogColumn;
-import com.limitart.net.binary.message.MessageMeta;
-
-public abstract class AbstractMessageLog extends MessageMeta implements ILog {
-	@LogColumn(type = SqlColumnType.MYSQL_bigint, comment = "记录时间")
-	public long createTime = System.currentTimeMillis();
-}
