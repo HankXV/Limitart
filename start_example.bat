d22de364d0c07e16edca0084834230ce73f7f0da
HankXV <104381832@qq.com>
2017-08-04 13:30:13 +0800

    add oom dump

--- start_example.bat
+++ start_example.bat
@@ -1,3 +1,3 @@
 set program_name=YourJarName
-java -jar %program_name%.jar -Dfile.encoding=UTF-8 -server -Xmx2g -Xms2g -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+UseConcMarkSweepGC -XX: ParallelRefProcEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -XX:+ExplicitGCInvokesConcurrent -Xloggc:./logs/GC.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails
+java -jar %program_name%.jar -Dfile.encoding=UTF-8 -server -Xmx2g -Xms2g -Xmn256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+UseConcMarkSweepGC -XX: ParallelRefProcEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -XX:+ExplicitGCInvokesConcurrent -XX:+HeapDumpOnOutOfMemoryError -Xloggc:./logs/GC.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails
 PAUSE
