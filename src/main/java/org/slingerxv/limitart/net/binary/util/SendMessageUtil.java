0508926a5d6e666437021d6d23263435248f23a0
HankXV <104381832@qq.com>
2017-08-15 14:59:41 +0800

    message flow

--- src/main/java/org/slingerxv/limitart/net/binary/util/SendMessageUtil.java
+++ src/main/java/org/slingerxv/limitart/net/binary/util/SendMessageUtil.java
@@ -17,7 +17,11 @@ package org.slingerxv.limitart.net.binary.util;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.slingerxv.limitart.collections.FrequencyReadRankMap;
+import org.slingerxv.limitart.collections.define.IRankMap;
 import org.slingerxv.limitart.funcs.Proc3;
 import org.slingerxv.limitart.funcs.Procs;
 import org.slingerxv.limitart.net.binary.codec.AbstractBinaryEncoder;
@@ -28,8 +32,15 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.handler.codec.string.LineSeparator;
 
 public final class SendMessageUtil {
+	public static boolean IS_FLOW = true;
+	private static FlowComparator COMPARATOR = new FlowComparator();
+	private static ConcurrentHashMap<Class<? extends Message>, Integer> FLOW_MIN = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<Class<? extends Message>, Integer> FLOW_MAX = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<Class<? extends Message>, Integer> FLOW_COUNT = new ConcurrentHashMap<>();
+	private static ConcurrentHashMap<Class<? extends Message>, Long> FLOW_SIZE = new ConcurrentHashMap<>();
 
 	private SendMessageUtil() {
 	}
@@ -55,6 +66,7 @@ public final class SendMessageUtil {
 		}
 		msg.buffer(null);
 		encoder.afterWriteBody(buffer);
+		flow(msg.getClass(), buffer);
 		channel.writeAndFlush(buffer).addListener((ChannelFutureListener) arg0 -> {
 			Procs.invoke(listener, arg0.isSuccess(), arg0.cause(), arg0.channel());
 		});
@@ -83,12 +95,102 @@ public final class SendMessageUtil {
 						channel);
 				continue;
 			}
-			channel.writeAndFlush(buffer.retainedSlice()).addListener((ChannelFutureListener) arg0 -> {
+			ByteBuf retainedSlice = buffer.retainedSlice();
+			flow(msg.getClass(), retainedSlice);
+			channel.writeAndFlush(retainedSlice).addListener((ChannelFutureListener) arg0 -> {
 				Procs.invoke(listener, arg0.isSuccess(), arg0.cause(), arg0.channel());
 			});
 			if (i == channels.size() - 1) {
 				buffer.release();
 			}
 		}
+	}
+
+	/**
+	 * 消息统计
+	 * 
+	 * @param class1
+	 * @param retainedSlice
+	 */
+	private static void flow(Class<? extends Message> clazz, ByteBuf buf) {
+		if (!IS_FLOW) {
+			return;
+		}
+		FLOW_MIN.putIfAbsent(clazz, Integer.MAX_VALUE);
+		FLOW_MAX.putIfAbsent(clazz, 0);
+		FLOW_COUNT.putIfAbsent(clazz, 0);
+		FLOW_SIZE.putIfAbsent(clazz, 0L);
+		int readableBytes = buf.readableBytes();
+		FLOW_MIN.put(clazz, Math.min(FLOW_MIN.get(clazz), readableBytes));
+		FLOW_MAX.put(clazz, Math.max(FLOW_MAX.get(clazz), readableBytes));
+		FLOW_COUNT.put(clazz, FLOW_COUNT.get(clazz) + 1);
+		FLOW_SIZE.put(clazz, FLOW_SIZE.get(clazz) + readableBytes);
+	}
+
+	/**
+	 * 生成流量报告
+	 * 
+	 * @return
+	 */
+	public static String reportFlow(int top) {
+		if (!IS_FLOW) {
+			return "no flow to report";
+		}
+		if (top < 1) {
+			return "top error!";
+		}
+		IRankMap<Class<? extends Message>, FlowMeta> min = new FrequencyReadRankMap<>(COMPARATOR, top);
+		IRankMap<Class<? extends Message>, FlowMeta> max = new FrequencyReadRankMap<>(COMPARATOR, top);
+		IRankMap<Class<? extends Message>, FlowMeta> count = new FrequencyReadRankMap<>(COMPARATOR, top);
+		IRankMap<Class<? extends Message>, FlowMeta> size = new FrequencyReadRankMap<>(COMPARATOR, top);
+		for (Entry<Class<? extends Message>, Integer> entry : FLOW_MIN.entrySet()) {
+			FlowMeta meta = new FlowMeta();
+			meta.setClazz(entry.getKey());
+			meta.setValue(entry.getValue());
+			min.put(entry.getKey(), meta);
+		}
+		for (Entry<Class<? extends Message>, Integer> entry : FLOW_MAX.entrySet()) {
+			FlowMeta meta = new FlowMeta();
+			meta.setClazz(entry.getKey());
+			meta.setValue(entry.getValue());
+			max.put(entry.getKey(), meta);
+		}
+		for (Entry<Class<? extends Message>, Integer> entry : FLOW_COUNT.entrySet()) {
+			FlowMeta meta = new FlowMeta();
+			meta.setClazz(entry.getKey());
+			meta.setValue(entry.getValue());
+			count.put(entry.getKey(), meta);
+		}
+		for (Entry<Class<? extends Message>, Long> entry : FLOW_SIZE.entrySet()) {
+			FlowMeta meta = new FlowMeta();
+			meta.setClazz(entry.getKey());
+			meta.setValue(entry.getValue());
+			size.put(entry.getKey(), meta);
+		}
+		List<FlowMeta> minRange = min.getRange(0, top);
+		List<FlowMeta> maxRange = max.getRange(0, top);
+		List<FlowMeta> countRange = count.getRange(0, top);
+		List<FlowMeta> sizeRange = size.getRange(0, top);
+		StringBuilder sb = new StringBuilder();
+		sb.append("=======min:").append(LineSeparator.DEFAULT.value());
+		for (FlowMeta meta : minRange) {
+			sb.append(meta.toString()).append(LineSeparator.DEFAULT.value());
+		}
+
+		sb.append("=======max:").append(LineSeparator.DEFAULT.value());
+		for (FlowMeta meta : maxRange) {
+			sb.append(meta.toString()).append(LineSeparator.DEFAULT.value());
+		}
+
+		sb.append("=======count:").append(LineSeparator.DEFAULT.value());
+		for (FlowMeta meta : countRange) {
+			sb.append(meta.toString()).append(LineSeparator.DEFAULT.value());
+		}
+
+		sb.append("=======size:").append(LineSeparator.DEFAULT.value());
+		for (FlowMeta meta : sizeRange) {
+			sb.append(meta.toString()).append(LineSeparator.DEFAULT.value());
+		}
+		return sb.toString();
 	}
 }
