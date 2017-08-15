4451973016529385ffb1589638d157009a018bb2
HankXV <104381832@qq.com>
2017-08-15 14:59:25 +0800

    jvm heap suggest

--- start_example.sh
+++ start_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 program_name=YourJarName
-nohup java -jar ${program_name}.jar -Dfile.encoding=UTF-8 -server -Xmx2g -Xms2g -Xmn768m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+UseConcMarkSweepGC -XX: ParallelRefProcEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -XX:+ExplicitGCInvokesConcurrent -XX:+HeapDumpOnOutOfMemoryError -Xloggc:./logs/GC.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails > nohup.txt &
+nohup java -jar ${program_name}.jar -Dfile.encoding=UTF-8 -server -Xmx2g -Xms2g -Xmn1g -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=768m -XX:+UseConcMarkSweepGC -XX: ParallelRefProcEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -XX:+ExplicitGCInvokesConcurrent -XX:+HeapDumpOnOutOfMemoryError -Xloggc:./logs/GC.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails > nohup.txt &
 echo $! > ${program_name}.pid
 tail -f nohup.txt
