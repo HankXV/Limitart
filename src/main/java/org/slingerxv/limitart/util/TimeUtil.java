e3c01053f50eb1d939c8ef1f5da2b578e66c6951
HankXV <104381832@qq.com>
2017-08-21 16:02:06 +0800

    zero days

--- src/main/java/org/slingerxv/limitart/util/TimeUtil.java
+++ src/main/java/org/slingerxv/limitart/util/TimeUtil.java
@@ -173,4 +173,20 @@ public final class TimeUtil {
 		String format2 = format.format(temp);
 		return format2;
 	}
+
+	/**
+	 * 获取所在天的凌晨时间
+	 * 
+	 * @param time
+	 * @return
+	 */
+	public long getZeroStamp(long time) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(time);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar.getTimeInMillis();
+	}
 }
