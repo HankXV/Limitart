1dee6ff2e2eee2c13882b4f2dcd8714ae367d65c
HankXV <104381832@qq.com>
2017-09-01 18:26:30 +0800

    change return value

--- src/main/java/org/slingerxv/limitart/util/FTPUtil.java
+++ src/main/java/org/slingerxv/limitart/util/FTPUtil.java
@@ -17,8 +17,8 @@ package org.slingerxv.limitart.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -66,9 +66,9 @@ public final class FTPUtil {
 		return null;
 	}
 
-	public static List<byte[]> download(String url, int port, String username, String password, String remotePath,
-			String dirName, String filePattern) throws IOException {
-		List<byte[]> result = new ArrayList<>();
+	public static Map<String, byte[]> download(String url, int port, String username, String password,
+			String remotePath, String dirName, String filePattern) throws IOException {
+		Map<String, byte[]> result = new HashMap<>();
 		FTPClient ftp = new FTPClient();
 		ftp.setConnectTimeout(5000);
 		ftp.setAutodetectUTF8(true);
@@ -89,7 +89,7 @@ public final class FTPUtil {
 				if (ff.getName().endsWith("." + filePattern)) {
 					try (ByteArrayOutputStream is = new ByteArrayOutputStream();) {
 						ftp.retrieveFile(ff.getName(), is);
-						result.add(is.toByteArray());
+						result.put(ff.getName(), is.toByteArray());
 					}
 				}
 			}
